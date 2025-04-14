e3c81b5d6eca902a1e8eaf13eca5bf414c7d40a7
shipleyux <sarashipley1@gmail.com>
2025-04-14 13:20:09 +0100

    "Set up Cloudinary, static/media folders, and templates in settings"

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'blog',
+
 ]
 
 MIDDLEWARE = [
