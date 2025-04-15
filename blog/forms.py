78282bb6b605c4aef225a28f8197d8f059e888ea
shipleyux <sarashipley1@gmail.com>
2025-04-15 16:06:48 +0100

    create register page

---
+++ blog/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')  # we’ll add login next
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
--- blog/urls.py
+++ blog/urls.py
@@ -21,5 +21,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('register/', views.register, name='register'),
 ]
 
