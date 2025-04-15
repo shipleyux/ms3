8bf86e28d28e8053196cc4c17da52f6d96a31ef3
shipleyux <sarashipley1@gmail.com>
2025-04-15 11:45:39 +0100

    Add gunicorn to requirements for Heroku

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# blog/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),  # routes here for specific views
 ]
 
