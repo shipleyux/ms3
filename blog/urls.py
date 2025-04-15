9f57165acd6331dffb2d1860bc0e0cc29a727eaf
shipleyux <sarashipley1@gmail.com>
2025-04-15 14:15:34 +0100

    page set up

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),  # routes here for specific views
+    path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import Post  # import your model
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
