9bb510f77133096b4e81e13dd6ad0e0732691066
shipleyux <sarashipley1@gmail.com>
2025-04-22 13:24:04 +0100

    fix migration issues

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
