dd2fbefdfa855474df77eb136d255fefd531797e
shipleyux <sarashipley1@gmail.com>
2025-04-29 14:52:09 +0100

    add categories

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Post
 from .models import Profile
 from .models import Comment, Post
+from .models import Post, Category
 
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Comment)
+admin.site.register(Category)
--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from .models import Post, Category
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -18,9 +19,14 @@ class RegisterForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'category']
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'category']
